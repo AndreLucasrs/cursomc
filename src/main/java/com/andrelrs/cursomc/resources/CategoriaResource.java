d2bd3fd91360d86cc238b916def207afc15a591e
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-26 18:44:51 -0300

    Listando todas categorias com DTO

--- src/main/java/com/andrelrs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andrelrs/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.andrelrs.cursomc.resources;
 
 import com.andrelrs.cursomc.domain.Categoria;
+import com.andrelrs.cursomc.dto.CategoriaDTO;
 import com.andrelrs.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -45,4 +48,22 @@ public class CategoriaResource {
 
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
+    }
+
 }
--- src/main/java/com/andrelrs/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/andrelrs/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,25 +1,33 @@
 package com.andrelrs.cursomc.resources.exception;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.andrelrs.cursomc.services.exceptions.DataIntegrityException;
+import com.andrelrs.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.andrelrs.cursomc.services.exceptions.ObjectNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 
 //Classe auxiliar que ira interceptar as excessoes e ela obrigatoriamente dentro do framework ela tem que ter essa assinatura no metodo
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
-	//Isso indica que ele é um tratador de excessao desse tipo de excessao
-	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNoutFound(ObjectNotFoundException e, HttpServletRequest request){
-		
-		StandardError error =  new StandardError(HttpStatus.NOT_FOUND.value(), 	e.getMessage(), System.currentTimeMillis());
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-	}
+
+    //Isso indica que ele é um tratador de excessao desse tipo de excessao
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNoutFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+        StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 
 }
