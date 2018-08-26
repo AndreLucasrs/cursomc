0c028f850a90994819fdc5033e075800660d85bb
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-26 17:48:06 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/andrelrs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andrelrs/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,38 @@
 package com.andrelrs.cursomc.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.andrelrs.cursomc.domain.Categoria;
 import com.andrelrs.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@Autowired
-	private CategoriaService service;
 
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
-		Categoria obj = service.find(id);
-		
-		return ResponseEntity.ok().body(obj);
-	}
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+
+        Categoria obj = service.find(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    //@RequestBody serve para converter o json em objeto java
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> inserir(@RequestBody Categoria obj) {
+        obj = service.inserir(obj);
+        //Depois de salvar o Objeto ele sera redirecionado, ele vai ser redirecionado para /{id},
+        // o id passada sera o que acabou de ser criado no objeto
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
 }
--- src/main/java/com/andrelrs/cursomc/services/CategoriaService.java
+++ src/main/java/com/andrelrs/cursomc/services/CategoriaService.java
@@ -1,24 +1,29 @@
 package com.andrelrs.cursomc.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.andrelrs.cursomc.domain.Categoria;
 import com.andrelrs.cursomc.repositories.CategoriaRepository;
 import com.andrelrs.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
 
-	@Autowired
-	private CategoriaRepository repo;
+    @Autowired
+    private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		//ele irá retorna o objeto ou a excessão
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        //ele irá retorna o objeto ou a excessão
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria inserir(Categoria obj) {
+		obj.setId(null);
+        return repo.save(obj);
+    }
 }
