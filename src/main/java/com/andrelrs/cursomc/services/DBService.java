6a19b9df86a0b3e3eb946d30734724237b21e3e5
AndreLucasrs <andrelrs.v@outlook.com>
2019-01-24 01:00:58 -0200

    Criando profile de teste

---
+++ src/main/java/com/andrelrs/cursomc/services/DBService.java
@@ -0,0 +1,138 @@
+package com.andrelrs.cursomc.services;
+
+import com.andrelrs.cursomc.domain.*;
+import com.andrelrs.cursomc.domain.enums.EstadoPagamento;
+import com.andrelrs.cursomc.domain.enums.TipoCliente;
+import com.andrelrs.cursomc.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    public void instantiateTestDatabase() throws ParseException {
+
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+        Categoria categoria3 = new Categoria(null, "Cama mesa e banho");
+        Categoria categoria4 = new Categoria(null, "Eletrônicos");
+        Categoria categoria5 = new Categoria(null, "Jardinagem");
+        Categoria categoria6 = new Categoria(null, "Decoração");
+        Categoria categoria7 = new Categoria(null, "Perfumaria");
+
+        Produto produto1 = new Produto(null, "Computador", 2000.00);
+        Produto produto2 = new Produto(null, "Impressora", 800.00);
+        Produto produto3 = new Produto(null, "Mouse", 80.00);
+        Produto produto4 = new Produto(null, "Mesa de escritório", 300.00);
+        Produto produto5 = new Produto(null, "Toalha", 50.00);
+        Produto produto6 = new Produto(null, "Colcha", 200.00);
+        Produto produto7 = new Produto(null, "TV true color", 1200.00);
+        Produto produto8 = new Produto(null, "Roçadeira", 800.00);
+        Produto produto9 = new Produto(null, "Abajour", 100.00);
+        Produto produto10 = new Produto(null, "Pendente", 180.00);
+        Produto produto11 = new Produto(null, "Shampoo", 90.00);
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlandia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "11111111111", TipoCliente.PESSOAFISICA);
+        cli1.getTelefones().addAll(Arrays.asList("12345678", "87654321"));
+
+        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 300", "Jardim", "38220834", cli1, c1);
+        Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "38777012", cli1, c2);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        ItemPedido ip1 = new ItemPedido(ped1, produto1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, produto3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, produto2, 100.00, 1, 800.00);
+
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItens().addAll(Arrays.asList(ip3));
+
+        produto1.getItens().addAll(Arrays.asList(ip1));
+        produto2.getItens().addAll(Arrays.asList(ip3));
+        produto3.getItens().addAll(Arrays.asList(ip2));
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+        est1.getCidades().add(c1);
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+        categoria2.getProdutos().addAll(Arrays.asList(produto2, produto4));
+        categoria3.getProdutos().addAll(Arrays.asList(produto5, produto6));
+        categoria4.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3, produto7));
+        categoria5.getProdutos().addAll(Arrays.asList(produto8));
+        categoria6.getProdutos().addAll(Arrays.asList(produto9, produto10));
+        categoria7.getProdutos().addAll(Arrays.asList(produto11));
+
+        produto1.getCategorias().addAll(Arrays.asList(categoria1, categoria4));
+        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2, categoria4));
+        produto3.getCategorias().addAll(Arrays.asList(categoria1, categoria4));
+        produto4.getCategorias().addAll(Arrays.asList(categoria2));
+        produto5.getCategorias().addAll(Arrays.asList(categoria3));
+        produto6.getCategorias().addAll(Arrays.asList(categoria3));
+        produto7.getCategorias().addAll(Arrays.asList(categoria4));
+        produto8.getCategorias().addAll(Arrays.asList(categoria5));
+        produto9.getCategorias().addAll(Arrays.asList(categoria6));
+        produto10.getCategorias().addAll(Arrays.asList(categoria6));
+        produto11.getCategorias().addAll(Arrays.asList(categoria7));
+
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
+        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+    }
+
+}
